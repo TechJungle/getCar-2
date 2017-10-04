d37deb97cebe993df171d19f788919a36b23e035
ElectricFiras <30745903+ElectricFiras@users.noreply.github.com>
2017-10-04 14:35:27 +0300

    Now with comments

--- client/components/logIns.js
+++ client/components/logIns.js
@@ -1,16 +1,28 @@
 angular.module('get-car')
 
+/*
+              ________
+             / ______ \
+             || _  _ ||
+             ||| || |||
+             |||_||_|||
+             || _  _o|| (o)
+             ||| || |||
+             |||_||_|||      ^~^  , Loop ?!
+             ||______||     ('Y') )
+            /__________\    /   \/
+    ________|__________|__ (\|||/) _________
+           /____________\
+           |____________|
+*/
 .component('logIns', {
 	controller: function ($scope, $http){
 	  $scope.submit = function(){
-      
       $scope.user = {user : $scope.username , password : $scope.password};
-        console.log($scope.user + "========")
       $http.post("/logIn", $scope.user).then(function(data){
-        console.log(data + "======+++++++++++++=")
         window.location = "../../index.html"
-    })
-  }
+    });
+  };
 	},
 	templateUrl: `
     client/templates/logIns.html
--- db/carDB.js
+++ db/carDB.js
@@ -1,3 +1,13 @@
+/*
+                                 _..-------++._
+                             _.-'/ |      _||  \"--._
+                       __.--'`._/_\j_____/_||___\    `----.
+                  _.--'_____    |          \     _____    /
+                _j    /,---.\   |        =o |   /,---.\   |_
+               [__]==// .-. \\==`===========/==// .-. \\=[__]
+                 `-._|\ `-' /|___\_________/___|\ `-' /|_.'     
+                       `---'                     `---'
+*/
 var mongoose = require("mongoose");
 var mongoURI = "mongodb://localHost/doaa";
 // var mongoURI = "mongodb://techJungle:techJungle5@ds159344.mlab.com:59344/get-car";
@@ -9,7 +19,7 @@ var carSchema = new Schema({
     type : {type: String, uppercase: true},
     color: {type: String, uppercase: true},
     price:String
-})
+});
 var carModel = mongoose.model('carModel',carSchema);
 
 mongoose.connect(mongoURI,{useMongoClient: true});
@@ -17,5 +27,5 @@ db.once("open", function(){
    console.log("connect to db");
  }).on("error", function(){
    console.log("ERROR CONNECT to db");
- })
+ });
 module.exports = carModel;
